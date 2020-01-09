f6d79271a9f57c85dfb1c8edd67c56e9343c00be
Bess Ashe <bashe@ox-t6.21-school.ru>
2020-01-09 21:52:29 +0300

    start

--- src/flags.c
+++ src/flags.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   flags.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bashe <bashe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/28 17:14:10 by bashe             #+#    #+#             */
-/*   Updated: 2020/01/09 20:18:13 by bashe            ###   ########.fr       */
+/*   Created: 2020/01/09 19:35:38 by bashe             #+#    #+#             */
+/*   Updated: 2020/01/09 20:17:22 by bashe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include "libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
+#include "ft_printf.h"
 
-int     ft_printf(const char * restrict format, ...);
-int		flag_check(va_list argptr, int *numberOfElement, char *str);
-
-#endif
+int		flag_check(va_list argptr, int *numberOfElement, char *str)
+{
+	
+}
--- src/ft_prinft.c
+++ src/ft_prinft.c
@@ -6,9 +6,39 @@
 /*   By: bashe <bashe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/28 17:18:51 by bashe             #+#    #+#             */
-/*   Updated: 2019/12/28 17:19:05 by bashe            ###   ########.fr       */
+/*   Updated: 2020/01/09 21:52:16 by bashe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int ft_printf(const char * restrict format, ...)
+{
+    va_list		argptr;
+    char		*str;
+	int			numberOfElement;
+
+	va_start(argptr, format);
+	str = (char *)format;
+	numberOfElement = 0;
+	while (*str != '\0')
+    {
+		if (*str == '%')
+			flag_check(argptr, &numberOfElement, str);
+		else
+		{
+			ft_putchar(*str);
+			numberOfElement++;
+		}
+	}
+	va_end(argptr);
+	return (0);
+}
+
+int main()
+{
+    ft_printf("bruh");
+	
+
+    return (0);
+}
